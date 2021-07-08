4aa7ba3bd7dc602cb888857ee3fa7e16ff5b46c4
j-codepro <j_code@icloud.com>
2021-07-09 00:07:35 +0200

    Five rounds game

--- script.js
+++ script.js
@@ -3,13 +3,11 @@ let playerSelection;
 let scorePlayer = 0;
 let scoreComputer = 0;
 
-// computer choice
 const computerChoices = ['Rock', 'Paper', 'Scissors'];
 function computerPlay() {
     return computerChoices[Math.floor(Math.random() * computerChoices.length)];
 };
 
-// calculate who is the winner of the round and add score
 function playRound(playerSelection, computerSelection) {
     computerSelection = computerPlay().toUpperCase();
     playerSelection = playerSelection.toUpperCase();
@@ -17,63 +15,48 @@ function playRound(playerSelection, computerSelection) {
         playerSelection == 'ROCK' && computerSelection === 'PAPER' ||
         playerSelection == 'PAPER' && computerSelection === 'SCISSORS') {
             scoreComputer++;
-            return displayResults(`You lose! ${computerSelection} beats ${playerSelection}. 
-            You ${scorePlayer} - Computer ${scoreComputer}`);
+            displayResults(`You lose! ${computerSelection} beats ${playerSelection}. 
+            You ${scorePlayer} - ${scoreComputer} Computer`);
     } else if 
         (playerSelection == 'SCISSORS' && computerSelection === 'PAPER' ||
          playerSelection == 'ROCK' && computerSelection === 'SCISSORS' ||
          playerSelection == 'PAPER' && computerSelection === 'ROCK') {
             scorePlayer++;
-            return displayResults(`You win! ${playerSelection} beats ${computerSelection}. 
-            You ${scorePlayer} - Computer ${scoreComputer}`);
+            displayResults(`You win! ${playerSelection} beats ${computerSelection}. 
+            You ${scorePlayer} - ${scoreComputer} Computer`);
     } else {
-        return displayResults(`No winner. You ${scorePlayer} - Computer ${scoreComputer}`);
+        displayResults(`No winner. You ${scorePlayer} - ${scoreComputer} Computer`);
+    }
+};
+
+function displayResults(strResult) {
+    const result = document.querySelector('.results');
+    result.textContent = strResult;
+};
+
+function displayFinalResults(strFinResult) {
+    const finalResult = document.querySelector('.finalResults');
+    finalResult.textContent = strFinResult;
+}
+
+function isWinner () {
+    if (scorePlayer > scoreComputer) {
+        displayFinalResults(`You won the game! You ${scorePlayer} - ${scoreComputer} Computer`);
+    } else if (scoreComputer < scorePlayer) {
+        displayFinalResults(`You lose the game... You ${scorePlayer} - ${scoreComputer} Computer`);
+    } else {
+        displayFinalResults(`Equality. You ${scorePlayer} - ${scoreComputer} Computer`);
     }
 };
 
 const btns = document.querySelectorAll("button");
 btns.forEach(button => button.addEventListener('click', () => {
     playerSelection = button.innerText.toLowerCase(); 
-    console.log(playRound(playerSelection, computerSelection));
+    playRound(playerSelection, computerSelection);
+    if (scorePlayer === 5 || scoreComputer === 5) {
+        isWinner();
+        scoreComputer = 0;
+        scorePlayer = 0;
+    }
   })
 );
-
-
-function displayResults(strResult) {
-    const result = document.querySelector('.results')
-    result.textContent = strResult;
-};
-
-
-// declare who is the winner of the game 
-/*
-function isWinner () {
-    if (scorePlayer > scoreComputer) {
-        return console.log(`Result: You won the game! You ${scorePlayer} - Computer ${scoreComputer}`);
-    } else if (scorePlayer < scoreComputer) {
-        return console.log(`Result: You lose the game... You ${scorePlayer} - Computer ${scoreComputer}`);
-    } else {
-        return console.log(`Result: Equality. You ${scorePlayer} - Computer ${scoreComputer}`);
-    }
-};
-*/
-
-/*
-
-// start the five rounds game
-function game() {
-    console.log('Round 1');
-    console.log(playRound());
-    console.log('Round 2');
-    console.log(playRound());
-    console.log('Round 3');
-    console.log(playRound());
-    console.log('Round 4');
-    console.log(playRound());
-    console.log('Round 5');
-    console.log(playRound());
-    isWinner();
-    return 'End of the game.';
-}
-
-*/
